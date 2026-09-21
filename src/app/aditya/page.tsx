6323c5922f4c370346b423ffbc00448dc78fa1e7
Aditya Sen <adityasen120@gmail.com>
2026-09-21 14:18:05 -0400

    add initial landing

---
+++ src/app/aditya/page.tsx
@@ -0,0 +1,75 @@
+import HexTextAnimation from '@/components/HexAnimation/HexAnimation';
+import Image from 'next/image';
+import logo from '@/imgs/profile.jpg';
+import Background from '@/components/Background/Background';
+import Projects from '@/components/Projects/Projects';
+import Career from '@/components/Career/Career';
+import About from '@/components/About/About';
+import Contact from '@/components/Contact/Contact';
+import '../styles/background.css';
+import '../styles/styles.css';
+
+
+export default function AdityaPage() {
+  return (
+    <div>
+      <Background />
+      <div 
+        className='container my-5'
+        data-bs-spy="scroll"
+        data-bs-target="#navbarNav"
+        data-bs-offset="80"
+        tabIndex={0}
+      >
+        <div className='row'>
+          <div className='col text-center text-md-start mb-4'>
+            <div className="profile-image-wrapper" data-augmented-ui="r-clip-y both">
+              <Image
+                src={logo}
+                alt="Aditya Sen"
+                width={350}
+                height={350}
+                style={{
+                  objectFit: 'cover',
+                }}
+                sizes="(max-width: 768px) 100vw, 350px"
+                priority={true}
+              />
+            </div>
+
+          </div>
+
+          <div className='col d-flex flex-column justify-content-center align-items-center text-center'>
+            <p className='fs-1 name'>Aditya Sen</p>
+
+            <div 
+              className='row py-2 text-center landing-subtext-container w-75'
+              data-augmented-ui="bl-clip-y tr-clip-y border"
+            >
+              <HexTextAnimation text='Graphics & Rendering Engineer' className='col responsive-border landing-subtext mb-0' duration={2} delay={0.5}/>
+            </div>
+          </div>
+        </div>
+
+
+        <div className='mt-5' id="projects">
+          <Projects />
+        </div>
+
+        <div className='mt-5'  id="career">
+          <Career />
+        </div>
+
+        <div className='mt-5' id="about">
+          <About />
+        </div>
+
+        <div className='mt-5' id="contact">
+          <Contact />
+        </div>
+      </div>
+
+      
+    </div>
+  );
+}
